3718afe2c7d5c4fc7a8890c6d46935b8423a3d7c
Phainsworth <128619260+Phainsworth@users.noreply.github.com>
2025-09-08 10:22:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3977,7 +3977,7 @@ function toneForROI(roi?: number) {
 
 
 <div className="text-center text-neutral-600 text-xs pb-10 relative z-10">
-  This tool is in beta - AI May Make Mistakes - Check Important info -  Not financial advice
+  This Tool Is In Beta - AI May Make Mistakes - Check Important Info -  Not Financial Advice
 </div>
 
 {/* Floating “Key” legend — left side */}
